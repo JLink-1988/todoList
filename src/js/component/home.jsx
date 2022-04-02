f193fe7c6c399fa62424bbf1c4a611192cc6b099
JLink-1988 <yasmanyjaneirogonzalez@gmail.com>
2022-04-02 00:57:09 +0000

    todo list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [item, setItem] = useState("");
+	const [todoList, setTodolist] = useState([]);
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div>TODO LIST</div>
+			<div className="input-group">
+				<input
+					type="text"
+					className="form-control"
+					placeholder="Recipient's username"
+					onChange={(event) => {
+						setItem(event.target.value);
+					}}
+					value={item}
+				/>
+				<button
+					className="btn btn-outline-secondary"
+					type="button"
+					onClick={() => {
+						if (item.length > 0) {
+							setTodolist([...todoList, item]);
+							setItem("");
+						}
+					}}>
+					Button
+				</button>
+			</div>
+			<ul>
+				{todoList.map((task, index) => {
+					return (
+						<li key={index}>
+							{task}
+							<div
+								className="d-flex justify-content"
+								onClick={() => {
+									setTodolist(
+										todoList.filter((el, i) => i !== index)
+									);
+								}}>
+								❌
+							</div>
+						</li>
+					);
+				})}
+			</ul>
 		</div>
 	);
 };
